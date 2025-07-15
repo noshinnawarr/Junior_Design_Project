5bf94969dc40036f8d6c82ecc582d51eb07bfefa
MubashshiraKaisar <107801533+MubashshiraKaisar@users.noreply.github.com>
2025-07-16 02:51:23 +0600

    Create image_dewarper.py

---
+++ Extracting_signature/image_dewarper.py
@@ -0,0 +1,28 @@
+import cv2
+import imutils
+import numpy as np
+# ❌ Remove this line:
+# from utils_transform import four_point_transform
+
+def dewarp_book(image):
+    ratio = image.shape[0] / 500.0
+    orig = image.copy()
+    image = imutils.resize(image, height=500)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    sigma = 0.33
+    v = np.median(image)
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+    warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+    return warped
